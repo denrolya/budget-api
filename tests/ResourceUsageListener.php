b8cd72e470b1bebcdda3d00a2b75f67596261272
Den Rolya <denrolya@gmail.com>
2024-10-12 23:05:03 +0200

    refactor(tests): add configuration to toggle on/off resource usage output for phpunit

--- tests/ResourceUsageListener.php
+++ tests/ResourceUsageListener.php
@@ -6,17 +6,27 @@ use PHPUnit\Runner\AfterTestHook;
 
 class ResourceUsageListener implements AfterTestHook
 {
+
+    private bool $enabled;
+
+    public function __construct(bool $enabled)
+    {
+        $this->enabled = $enabled;
+    }
+
     public function executeAfterTest(string $test, float $time): void
     {
-        $memoryUsage = memory_get_usage(true) / 1024 / 1024;
-        $peakMemoryUsage = memory_get_peak_usage(true) / 1024 / 1024;
+        if ($this->enabled) {
+            $memoryUsage = memory_get_usage(true) / 1024 / 1024;
+            $peakMemoryUsage = memory_get_peak_usage(true) / 1024 / 1024;
 
-        echo sprintf(
-            "\n[INFO] Test '%s' finished. Time: %.2f seconds. Memory usage: %.2f MB (Peak: %.2f MB)\n",
-            $test,
-            $time,
-            $memoryUsage,
-            $peakMemoryUsage
-        );
+            echo sprintf(
+                "\n[INFO] Test '%s' finished. Time: %.2f seconds. Memory usage: %.2f MB (Peak: %.2f MB)\n",
+                $test,
+                $time,
+                $memoryUsage,
+                $peakMemoryUsage
+            );
+        }
     }
 }
