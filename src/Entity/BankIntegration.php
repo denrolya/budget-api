1aaa71e3217df6f7eec998ba55faeb8783aa8b07
Den Rolya <denrolya@gmail.com>
2026-03-16 17:01:28 +0100

    refactor: restore missing bank integration fields

--- src/Entity/BankIntegration.php
+++ src/Entity/BankIntegration.php
@@ -211,6 +211,11 @@ class BankIntegration implements OwnableInterface
         return $this->isActive;
     }
 
+    public function getIsActive(): bool
+    {
+        return $this->isActive;
+    }
+
     public function setIsActive(bool $isActive): self
     {
         $this->isActive = $isActive;
